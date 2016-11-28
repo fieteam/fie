0b59baff65375acf95665bb9e21b45a5075df08b
zernmal <zernmal@foxmail.com>
2016-11-28 16:45:39 +0800

    break: 移除fie-error的register api

--- packages/fie-error/lib/index.js
+++ packages/fie-error/lib/index.js
@@ -1,7 +1,6 @@
 /**
  * @desc 异常处理中心,扑获常见的异常 并人性化展示
  * @requires fie-error/handle
- * @requires fie-error/register
  * @author 擎空 <zernmal@foxmail.com>
  * @namespace fie-error
  */
@@ -9,9 +8,7 @@
 'use strict';
 
 const handle = require('./handle');
-const register = require('./register');
 
 module.exports = {
-  handle,
-  register
+  handle
 };
