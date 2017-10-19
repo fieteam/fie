5bdb2060f7856f1fc99e2e5e46a7643af04147fb
hugohua <baofen14787@gmail.com>
2017-10-19 10:52:27 +0800

    feat: 新增全局配置

---
+++ packages/fie-core-config/lib/index.js
@@ -0,0 +1,16 @@
+/**
+ * @author 宇果 <baofen14787@gmail.com>
+ * @desc fie 运行时的全局配置
+ */
+
+'use strict';
+module.exports = {
+
+  /**
+   * 获取运行时的cli名称
+   */
+  getBinName(){
+    return process.env.FIE_BIN || 'fie';
+  }
+
+};
