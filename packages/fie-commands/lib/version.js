99eb5c98fb16242a6b38ef96771f57529bd159a7
hugohua <baofen14787@gmail.com>
2017-10-24 11:05:42 +0800

    feat: add -v show toolkit version

--- packages/fie-commands/lib/version.js
+++ packages/fie-commands/lib/version.js
@@ -7,6 +7,12 @@
 const path = require('path');
 const argv = require('yargs').help(false).argv;
 const chalk = require('chalk');
+const home = require('fie-home');
+const config = require('fie-config');
+const coreConfig = require('fie-core-config');
+const log = require('fie-log')('core-commands');
+
+const fieModuleName = require('fie-module-name');
 
 function getPackagesVersion() {
   let str = '';
@@ -28,7 +34,21 @@ function getPackagesVersion() {
 }
 
 module.exports = function* () {
-  console.log(chalk.magenta(`v${process.env.FIE_VERSION}`));
+  const bin = coreConfig.getBinName();
+
+  console.log(chalk.magenta(`${bin} v${process.env.FIE_VERSION}`));
+
+  //获取toolkit
+  let toolkitName = config.getToolkitName();
+  toolkitName = fieModuleName.toolkitFullName(toolkitName);
+  try{
+    const pkgPath = path.join(home.getModulesPath(),toolkitName,'package.json');
+    log.debug(`${toolkitName} pacage.json path = ${pkgPath}`);
+    const pkg = require( pkgPath );
+    console.log(chalk.magenta(`${toolkitName} v${pkg.version}`));
+  }catch (e){
+    log.debug(e);
+  }
 
   if (argv.d || argv.debug) {
     // 其依赖的核心包
