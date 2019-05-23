43ae270204618b05347fc9069e2334d707b80f53
le.wangl1 <le.wangl1@alibaba-inc.com>
2019-05-23 20:52:12 +0800

    feat: add rule cache; support exec rules async

--- packages/fie-rule-dispatcher/lib/index.js
+++ packages/fie-rule-dispatcher/lib/index.js
@@ -1,36 +1,64 @@
 
 'use strict';
 
-const path = require('path');
-const fs = require('fs-extra');
 const request = require('axios');
-const report = require('fie-report');
 const fieModule = require('fie-module');
-const shelljs = require('shelljs');
+const fieCache = require('fie-cache');
+const fieConfig = require('fie-config');
+const { exec } = require('child_process');
+
+const co = require('co');
 const log = require('fie-log')('fie-rule-dispatcher');
 
 // 先从环境变量里获取fie配置文件的目录，这样方便做调试
-const CWD = process.cwd();
-
+const cwd = process.cwd();
+const ruleDataCacheKey = 'dynamic-rules';
+const ruleDataCacheExpire = 3600000; // 1小时
 const publisherUrl = 'https://mocks.alibaba-inc.com/mock/fie-config-platform/config?_tag=%E6%B5%8B%E8%AF%95%E5%8B%BF%E8%BD%BB%E6%98%93%E5%8A%A8';
 
+// fie.config.js中的插件配置
+// forceUpdateRules: 强制更新数据
+// disableDynamicRules: 允许用户关闭动态规则执行
+
+const {
+  forceUpdateRules,
+  disableDynamicRules // 用于用
+} = fieConfig.get('rule-dispatcher') || {};
 let fieAllRules = {};
 
 async function fetchRulesOnline() {
   const resp = await request(publisherUrl);
   const _data = resp.data;
-  const { code, data } = _data;
+  const { data } = _data; // {code,data}
+
   return data;
 }
 
-function cacheRules() {}
+function saveRuleDataToCache(data) {
+  fieCache.set(ruleDataCacheKey, data, { expires: ruleDataCacheExpire });
+  log.debug('saveRuleDataToCache', data);
+  return true;
+}
+
+function readRuleDataFromCache() {
+  const cache = fieCache.get(ruleDataCacheKey);
+  log.debug('readRuleDataFromCache: ', cache);
+  return cache;
+}
 
 async function syncRules() {
-  log.debug('fetch rules from online');
-  const data = await fetchRulesOnline();
-  fieAllRules = data.rules || {};
-  // cache rules
-  // read rules
+  log.debug('syncRules');
+  let data = readRuleDataFromCache();
+  if (!data || forceUpdateRules) {
+    try {
+      data = await fetchRulesOnline();
+      saveRuleDataToCache(data);
+    } catch (e) {
+      log.error('fie同步规则出错', e);
+    }
+  }
+  fieAllRules = data ? data.rules : [];
+
   return fieAllRules;
 }
 
@@ -42,16 +70,40 @@ function getRules({ toolkit, command, preriod }) {
   return rules;
 }
 
-function installPluginsIfNeed(plugin) {
+async function installPluginsIfNeed(plugin) {
 // 按需安装fie 插件
-  fieModule.install(plugin);
+  const module = await (new Promise((resolve) => {
+    co(function* () {
+      const mod = yield fieModule.get(plugin);
+      resolve(mod);
+    });
+  }));
+  return module;
 }
 
 // 执行一段sh命令
-function execScript(script, async) {
-  log.debug(`execScript: ${script}`, `. async=${async}`);
-  const result = shelljs.exec(script, { silent: false, CWD });
-  const { code, stdout } = result;
+async function execScript({ script, async = false, silent = true }) {
+  log.debug(`execScript: ${script}`, `. async=${async}, silent=${silent}`);  // silent暂不实现.
+
+  let process = null;
+  if (async) {  // 异步
+    return new Promise(() => {
+      log.debug(`execScript, will spawn:  ${script}`);
+      process = exec(script, { cwd }, (err, stdout, stderr) => {
+        if (err) {
+          log.debug(`execScript执行命令出错, ${script}, ${err}`);
+        }
+        if (stdout) { log.debug(`execScript执行命令成功, ${script}, ${stdout}`); }
+        if (stderr) { log.debug(`execScript执行命令成功, ${script}, ${stderr}`); }
+      });
+
+      log.debug('exec script in child process:', process.pid);
+    });
+  }
+  // 同步
+  process = exec(script, { cwd }); // {silent,}
+  log.debug('exec script in child process:', process);
+  const { code, stdout } = process;
   const success = code === 0;
   if (!success) {
     log.debug(`execScript执行命令出错, ${script}`);
@@ -64,20 +116,28 @@ function execScript(script, async) {
 }
 
 function execRules({ toolkit, command, preriod }) {
+  if (disableDynamicRules) {
+    return;
+  }
+  log.debug(`execRules: toolkit=${toolkit}, command=${command}, preriod=${preriod} `);
   const rules = getRules({ toolkit, command, preriod });
-  if (!rules) return false;
-  rules.forEach((rule) => {
-    const { plugin, script, async, type = 'shell' } = rule;
+  log.debug(`and rules = : ${rules}`);
+  if (!rules) return;
+  rules.forEach(async (rule) => {
+    const { plugin, script, async, type = 'shell', silent } = rule;
     if (plugin) {
-      installPluginsIfNeed(plugin);
+      const fiePlugin = await installPluginsIfNeed(plugin);
+      log.debug('plugin existed or installed:', fiePlugin);
     }
     switch (type) {
       case 'shell': {
-        execScript(script, async); break;
+        execScript({ script, async, silent }); break;
       }
       case 'node':
         break;
-      default:execScript(script, async); break;
+      default: {
+        execScript({ script, async, silent });
+      }
     }
     return true;
   });
